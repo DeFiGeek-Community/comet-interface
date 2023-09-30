91c34a12854faee675216600b1193fdafdce9c06
naizo10 <naizo018767@gmail.com>
2023-09-30 18:05:59 +0900

    Added status bar loading display

--- components/pool/CollateralRatioBar.tsx
+++ components/pool/CollateralRatioBar.tsx
@@ -16,57 +16,55 @@ const CollateralRatioBar = ({ poolData }: { poolData?: PoolConfig }) => {
 
   return (
     <DashboardBox width="100%" height="65px" mt={4} p={4}>
-      <Row mainAxisAlignment="flex-start" crossAxisAlignment="center" expand>
-        <SimpleTooltip
-          label={t("Keep this bar from filling up to avoid being liquidated!")}
-        >
-          <Text flexShrink={0} mr={4}>
-            {t("Liquidation Limit")}
-          </Text>
-        </SimpleTooltip>
+      {basePoolData && positionSummary ? (
+        <Row mainAxisAlignment="flex-start" crossAxisAlignment="center" expand>
+          <SimpleTooltip
+            label={t("Keep this bar from filling up to avoid being liquidated!")}
+          >
+            <Text flexShrink={0} mr={4}>
+              {t("Liquidation Limit")}
+            </Text>
+          </SimpleTooltip>
 
-        {basePoolData && positionSummary ? (
-          <>
-            <SimpleTooltip label={t("This is how much you have borrowed.")}>
-              <Text flexShrink={0} mt="2px" mr={3} fontSize="10px">
-                {smallUsdFormatter(basePoolData.yourBorrow)}
-              </Text>
-            </SimpleTooltip>
+          <SimpleTooltip label={t("This is how much you have borrowed.")}>
+            <Text flexShrink={0} mt="2px" mr={3} fontSize="10px">
+              {smallUsdFormatter(basePoolData.yourBorrow)}
+            </Text>
+          </SimpleTooltip>
 
-            <SimpleTooltip
-              label={`You're using ${
-                positionSummary.LiquidationPercentage
-              }% of your ${smallUsdFormatter(
-                positionSummary.LiquidationPoint,
-              )} Liquidation limit.`}
-            >
-              <Box width="100%">
-                <Progress
-                  size="xs"
-                  width="100%"
-                  colorScheme={"whatsapp"}
-                  borderRadius="10px"
-                  value={positionSummary.LiquidationPercentage}
-                />
-              </Box>
-            </SimpleTooltip>
+          <SimpleTooltip
+            label={`You're using ${
+              positionSummary.LiquidationPercentage
+            }% of your ${smallUsdFormatter(
+              positionSummary.LiquidationPoint,
+            )} Liquidation limit.`}
+          >
+            <Box width="100%">
+              <Progress
+                size="xs"
+                width="100%"
+                colorScheme={"whatsapp"}
+                borderRadius="10px"
+                value={positionSummary.LiquidationPercentage}
+              />
+            </Box>
+          </SimpleTooltip>
 
-            <SimpleTooltip
-              label={t(
-                "If your borrow amount reaches this value, you will be liquidated.",
-              )}
-            >
-              <Text flexShrink={0} mt="2px" ml={3} fontSize="10px">
-                {smallUsdFormatter(positionSummary.LiquidationPoint)}
-              </Text>
-            </SimpleTooltip>
-          </>
-        ) : (
-          <Center height="50px">
-            <Spinner />
-          </Center>
-        )}
-      </Row>
+          <SimpleTooltip
+            label={t(
+              "If your borrow amount reaches this value, you will be liquidated.",
+            )}
+          >
+            <Text flexShrink={0} mt="2px" ml={3} fontSize="10px">
+              {smallUsdFormatter(positionSummary.LiquidationPoint)}
+            </Text>
+          </SimpleTooltip>
+        </Row>
+      ) : (
+        <Center height="30px">
+          <Spinner />
+        </Center>
+      )}
     </DashboardBox>
   );
 };
