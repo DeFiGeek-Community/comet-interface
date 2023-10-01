55b54ae1a6d1b5d96742f2906b465295ae5acccb
naizo10 <naizo018767@gmail.com>
2023-10-01 22:28:31 +0900

    Delete unnecessary imports

--- components/shared/StatsRow.tsx
+++ components/shared/StatsRow.tsx
@@ -1,28 +1,6 @@
-import React, { useState } from "react";
-import { useTranslation } from "react-i18next";
-import BigNumber from "bignumber.js";
-import {
-  Heading,
-  Box,
-  Button,
-  Text,
-  Image,
-  Input,
-  Tab,
-  TabList,
-  Tabs,
-  Spinner,
-} from "@chakra-ui/react";
-import { HashLoader } from "react-spinners";
-import { useBalance, useAccount } from "wagmi";
-import { smallUsdFormatter, smallFormatter } from "utils/bigUtils";
-import { Row, Column, useIsMobile, Center } from "utils/chakraUtils";
-import useBaseAssetData from "hooks/pool/indivisual/useBaseAsset";
-import useCollateralAssetData from "hooks/pool/indivisual/useCollateralAsset";
-import DashboardBox from "components/shared/DashboardBox";
-import { ModalDivider } from "components/shared/Modal";
-import { Mode } from "components/PoolModal";
-import { PoolConfig, BaseAsset, CollateralAsset } from "interfaces/pool";
+import React from "react";
+import { Text } from "@chakra-ui/react";
+import { Row } from "utils/chakraUtils";
 
 const StatsRow = ({
   label,
