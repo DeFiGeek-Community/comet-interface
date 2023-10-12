9c1899e9dcf1f71e8c5cd1bae9d74c4d3f8c0123
naizo10 <naizo018767@gmail.com>
2023-10-12 23:42:00 +0900

    Build useBaseAssetData

--- hooks/pool/indivisual/useBaseAsset.ts
+++ hooks/pool/indivisual/useBaseAsset.ts
@@ -1,5 +1,9 @@
-import { useState, useMemo } from "react";
+import { useState, useEffect } from "react";
 import { PoolConfig } from "interfaces/pool";
+import { getAccount, getContract, getNetwork, getWalletClient } from "@wagmi/core";
+import usePoolData from "hooks/pool/shared/usePoolConfig";
+import cometAbi from "statuc/comet.json";
+import { Address } from "abitype";
 
 interface BaseAssetData {
   supplyAPR: number | undefined;
@@ -9,46 +13,63 @@ interface BaseAssetData {
   availableToBorrow: number | undefined;
 }
 
+const getCometContract = async (address: Address) => {
+  const network = getNetwork();
+  const walletClient = await getWalletClient({ chainId: network.chain?.id });
+  return getContract({
+    address,
+    abi: cometAbi,
+    walletClient: walletClient as any,
+  });
+};
+
+const fetchDataFromComet = async (method: string): Promise<number | undefined> => {
+  const poolData = usePoolData();
+  if (!poolData) return undefined;
+  const comet = await getCometContract(poolData.proxy);
+  const { address } = getAccount();
+  if (!address) return undefined;
+  const data = await comet.read[method]([address]);
+  return data as number;
+};
+
+
+export const fetchSupplyAPR = async () => undefined;
+
+export const fetchYourSupply = async () => fetchDataFromComet("balanceOf");
+
+export const fetchBorrowAPR = async () => undefined;
+
+export const fetchYourBorrow = async () => fetchDataFromComet("borrowBalanceOf");
+
+export const fetchAvailableToBorrow = async () => undefined;
+
 const useBaseAssetData = (poolData: PoolConfig | undefined) => {
+  const [baseAssetData, setBaseAssetData] = useState<BaseAssetData>();
   const [error, setError] = useState<Error | null>(null);
   const [reloadKey, setReloadKey] = useState(0);
 
-  const baseAssetData = useMemo<baseAssetData | undefined>(() => {
-    if (!poolData) {
-      return undefined;
-    }
-
-    let fetchedData: baseAssetData | undefined;
-
+  useEffect(() => {
     const fetchBaseAsset = async () => {
       try {
-        // const availableToBorrow = getAvailableToBorrow();
+        const [supplyAPR, yourSupply, borrowAPR, yourBorrow, availableToBorrow] = await Promise.all([
+          fetchSupplyAPR(),
+          fetchYourSupply(),
+          fetchBorrowAPR(),
+          fetchYourBorrow(),
+          fetchAvailableToBorrow(),
+        ]);
 
-        // ダミーデータを使用
-        fetchedData = {
-          supplyAPR: 0,
-          yourSupply: 0,
-          borrowAPR: 0,
-          yourBorrow: 0,
-          availableToBorrow: 0,
-        };
+        setBaseAssetData({ supplyAPR, yourSupply, borrowAPR, yourBorrow, availableToBorrow });
       } catch (err) {
-        if (err instanceof Error) {
-          setError(err);
-        } else {
-          setError(new Error(String(err)));
-        }
+        setError(err instanceof Error ? err : new Error(String(err)));
       }
     };
 
     fetchBaseAsset();
-
-    return fetchedData;
   }, [poolData, reloadKey]);
 
-  const reload = () => {
-    setReloadKey((prevKey) => prevKey + 1);
-  };
+  const reload = () => setReloadKey(prevKey => prevKey + 1);
 
   return { baseAssetData, error, reload };
 };
