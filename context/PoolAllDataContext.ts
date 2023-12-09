226c43262d32c4c78ff2070b6e876061a08decb5
yosuke <yosuke2002jp@yahoo.co.jp>
2023-12-09 15:20:40 +0900

    add PoolAllDataContext.ts

---
+++ context/PoolAllDataContext.ts
@@ -0,0 +1,20 @@
+import { createContext } from "react";
+import { PriceFeedData } from "hooks/pool/shared/usePriceFeed";
+import { BaseAssetData } from "hooks/pool/indivisual/useBaseAsset";
+import { CollateralAssetsData } from "hooks/pool/indivisual/useCollateralAssets";
+import { TotalPoolData } from "hooks/pool/shared/useTotalPoolData";
+import { ClaimReward } from "hooks/pool/indivisual/useClaimReward";
+
+export interface PoolAllDataContextType {
+  priceFeedData: PriceFeedData | undefined;
+  baseAssetData: BaseAssetData | undefined;
+  collateralAssetsData: CollateralAssetsData | undefined;
+  totalPoolData: TotalPoolData | undefined;
+  claimReward: ClaimReward | undefined;
+}
+
+const PoolAllDataContext = createContext<
+PoolAllDataContextType | undefined
+>(undefined);
+
+export default PoolAllDataContext;
