79216fc47ad0afebb455e386ba43a713c8cc06a5
yosuke <yosuke2002jp@yahoo.co.jp>
2023-12-11 20:47:10 +0900

    quote part of the PoolTable in the PoolList

--- components/PoolList.tsx
+++ components/PoolList.tsx
@@ -19,6 +19,12 @@ import { POOL_CONFIG_MAP, SupportedPoolName } from "constants/pools";
 import { PoolConfig } from "interfaces/pool";
 import useTotalPoolData from "hooks/pool/shared/useTotalPoolData";
 
+import { Avatar} from "@chakra-ui/react";
+import { Heading, Text } from "@chakra-ui/react";
+import { Row, } from "utils/chakraUtils";
+import DashboardBox from "components/shared/DashboardBox";
+import { ModalDivider } from "components/shared/Modal";
+
 export interface TotalCollateralData {
   [key: string]: number | undefined;
 }
@@ -87,16 +93,112 @@ const PoolList = memo(() => {
             <Header />
             {/* <TabBar /> */}
             <StatsBar poolData={poolData} isPoolList={true} />
-            {poolData ? (
+            {/* {poolData ? (
               <PoolTable poolData={poolData} />
             ) : (
               <Center height="200px">
                 <Spinner />
               </Center>
-            )}
+            )} */}
             {/* {poolConfig?.map((asset, index) => {
               return (<PoolTable poolData={asset} />);
             })} */}
+            <DashboardBox
+      width="100%"
+      height="100%"
+      mt={4}
+      p={4}
+      fontSize="lm"
+      fontWeight="bold"
+    >
+      <Column
+        mainAxisAlignment="flex-start"
+        crossAxisAlignment="flex-start"
+        height="100%"
+        pb={1}
+      >
+        <Heading size="md" px={4} py={3}>
+          {t("Pool Lists")}
+        </Heading>
+        <ModalDivider />
+        <Row
+          mainAxisAlignment="flex-start"
+          crossAxisAlignment="flex-start"
+          width="100%"
+          height="50px"
+          px={4}
+          my={4}
+        >
+          <Row
+            mainAxisAlignment="flex-start"
+            crossAxisAlignment="flex-start"
+            width="40%"
+          >
+            <Column
+              mainAxisAlignment="flex-start"
+              crossAxisAlignment="flex-start"
+              width="100%"
+            >
+              <Row
+                mainAxisAlignment="flex-start"
+                crossAxisAlignment="flex-start"
+                width="100%"
+                mb={2}
+              >
+                <Text textAlign="center" fontWeight="bold" pl={1}>
+                  {t("Pool Assets")}
+                </Text>
+              </Row>
+              <Row
+                mainAxisAlignment="flex-start"
+                crossAxisAlignment="flex-start"
+                width="100%"
+              >
+                <Text width="25%" fontWeight="bold" pl={1}>
+                  {t("Base Asset")}
+                </Text>
+                <Text width="75%" fontWeight="bold" pl={1}>
+                  {t("Collateral Asset")}
+                </Text>
+              </Row>
+            </Column>
+          </Row>
+          <Row
+            mainAxisAlignment="center"
+            crossAxisAlignment="center"
+            height="100%"
+            width={isMobile ? "33%" : "20%"}
+          >
+            <Text textAlign="center" fontWeight="bold">
+            全預入残高{/* {t("Total {{symbol}} Supply Balance", { symbol })} */}
+            </Text>
+          </Row>
+          <Row
+            mainAxisAlignment="center"
+            crossAxisAlignment="center"
+            height="100%"
+            width={isMobile ? "33%" : "20%"}
+          >
+            <Text textAlign="center" fontWeight="bold">
+            全借入残高{/* {t("Total {{symbol}} Borrow Balance", { symbol })} */}
+            </Text>
+          </Row>
+          <Row
+            mainAxisAlignment="center"
+            crossAxisAlignment="center"
+            height="100%"
+            width={isMobile ? "33%" : "20%"}
+          >
+            <Text textAlign="center" fontWeight="bold">
+            全担保価値{/* {t("Total Collateral Balance")} */}
+            </Text>
+          </Row>
+        </Row>
+        <ModalDivider />
+        
+        <ModalDivider />
+      </Column>
+    </DashboardBox>
             <Footer />
           </Column>
         </PoolSecondaryDataProvider>
