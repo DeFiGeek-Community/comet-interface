54fce52c9ab51cffe7bf59fc8874c490cb628ab6
yosuke <yosuke2002jp@yahoo.co.jp>
2024-01-07 18:02:31 +0900

    modify pool.tsx copied index

--- pages/pool.tsx
+++ pages/pool.tsx
@@ -1,85 +1,51 @@
-import React, { memo, useEffect } from "react";
-import { useAccount } from "wagmi";
-import { Column, Center, useIsMobile } from "utils/chakraUtils";
-import { useReload } from "context/ReloadContext";
-import StatsBar from "components/pool/StatsBar";
-import Footer from "components/shared/Footer";
-import { Header } from "components/shared/Header";
-import { CurrencyProvider } from "components/Provider/currencyProvider";
-import { PoolPrimaryDataProvider } from "components/Provider/PoolPrimaryDataProvider";
-import { PoolSecondaryDataProvider } from "components/Provider/PoolSecondaryDataProvider";
-import usePoolData from "hooks/pool/shared/usePoolConfig";
-import { Spinner } from "@chakra-ui/react";
-import CollateralRatioBar from "components/pool/CollateralRatioBar";
-import BaseList from "components/pool/BaseList";
-import CollateralList from "components/pool/CollateralList";
-import ClaimReward from "components/pool/ClaimReward";
-import DashboardBox from "components/shared/DashboardBox";
+import React, { useState, useEffect } from "react";
+import { useNetwork } from "wagmi";
+import { HashLoader } from "react-spinners";
+import { Center } from "utils/chakraUtils";
+import PoolPage from "components/PoolPage";
+import { ReloadContextProvider } from "components/Provider/ReloadContextProvider";
+import { PoolContext } from "context/PoolContext";
+import { useRouter } from "next/router";
 
-const PoolContents = memo(() => {
-  const poolData = usePoolData();
-  const isMobile = useIsMobile();
-
-  const { address } = useAccount();
-  const { reload } = useReload();
+const Pool = () => {
+  const { chain } = useNetwork();
+  const router = useRouter();
+  const [chainId, setChainId] = useState<number>(chain?.id || 1);
+  const [poolName, setPoolName] = useState<string>("");
 
   useEffect(() => {
-    reload();
-  }, [address]);
+    if (chain) {
+      setChainId(chain.id);
+    }
+  }, [chain]);
+
+  useEffect(() => {
+    if (router.isReady) {
+      setPoolName(router.query.pool ? (router.query.pool as string) : "CJPY");
+    }
+  }, [router.isReady, router.query.pool]);
+
+  const [isRendered, setIsRendered] = useState(false);
+
+  useEffect(() => {
+    setIsRendered(true);
+  }, []);
 
   return (
-    <PoolPrimaryDataProvider poolData={poolData}>
-      <CurrencyProvider>
-        <PoolSecondaryDataProvider poolData={poolData}>
-          <Column
-            mainAxisAlignment="flex-start"
-            crossAxisAlignment="center"
-            color="#FFFFFF"
-            mx="auto"
-            width={isMobile ? "100%" : "1150px"}
-            px={isMobile ? 4 : 0}
-          >
-            <Header />
-            {/* <TabBar /> */}
-            <StatsBar poolData={poolData} />
-            <CollateralRatioBar poolData={poolData} />
-
-            <DashboardBox mt={4} width={"100%"}>
-              {poolData ? (
-                <BaseList poolData={poolData} />
-              ) : (
-                <Center height="200px">
-                  <Spinner />
-                </Center>
-              )}
-            </DashboardBox>
-
-            <DashboardBox ml={0} mt={4} width={"100%"}>
-              {poolData ? (
-                <CollateralList poolData={poolData} />
-              ) : (
-                <Center height="200px">
-                  <Spinner />
-                </Center>
-              )}
-            </DashboardBox>
-            <DashboardBox ml={0} mt={4} width={"100%"}>
-              {poolData ? (
-                <ClaimReward poolData={poolData} />
-              ) : (
-                <Center height="100px">
-                  <Spinner />
-                </Center>
-              )}
-            </DashboardBox>
-            <Footer />
-          </Column>
-        </PoolSecondaryDataProvider>
-      </CurrencyProvider>
-    </PoolPrimaryDataProvider>
+    <PoolContext.Provider
+      value={{ chainId, poolName, setChainId, setPoolName }}
+    >
+      <ReloadContextProvider>
+        {isRendered && router.isReady ? (
+          <PoolPage />
+        ) : (
+          <Center height="100vh">
+            <HashLoader color="#FFF" />
+          </Center>
+        )}
+      </ReloadContextProvider>
+    </PoolContext.Provider>
   );
-});
+};
 
-PoolContents.displayName = "PoolContents";
-
-export default PoolContents;
+export default Pool;
