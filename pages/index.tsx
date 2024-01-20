728ac55d467a7e0e2e0ff408afcdded21760fd1a
yosuke <yosuke2002jp@yahoo.co.jp>
2024-01-20 22:46:05 +0900

    remove useEffect in index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,23 +8,7 @@ import { PoolContext } from "context/PoolContext";
 import { useRouter } from "next/router";
 
 const Pool = () => {
-  const { chain } = useNetwork();
   const router = useRouter();
-  const [chainId, setChainId] = useState<number>(chain?.id || 1);
-  const [poolName, setPoolName] = useState<string>("");
-
-  useEffect(() => {
-    if (chain) {
-      setChainId(chain.id);
-    }
-  }, [chain]);
-
-  useEffect(() => {
-    if (router.isReady) {
-      setPoolName(router.query.pool ? (router.query.pool as string) : "CJPY");
-    }
-  }, [router.isReady, router.query.pool]);
-
   const [isRendered, setIsRendered] = useState(false);
 
   useEffect(() => {
