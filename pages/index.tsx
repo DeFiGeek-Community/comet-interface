107b12df01ceabe90a4ec2c71e2bdf17fb09a2b4
yosuke <yosuke2002jp@yahoo.co.jp>
2024-02-17 08:29:09 +0900

    remove //

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ const Pool = () => {
   const router = useRouter();
   const { setChainId, setPoolName } = useChainPool();
   const { chain } = useNetwork();
-//
+
   useEffect(() => {
     if (chain) {
       setChainId(chain.id);
