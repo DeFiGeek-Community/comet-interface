72191ad93edde6295528aac08eb6066cd19005dd
yosuke <yosuke2002jp@yahoo.co.jp>
2024-03-03 16:16:10 +0900

    remove comment in index

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,27 +3,15 @@ import { useNetwork } from "wagmi";
 import PoolList from "components/PoolList";
 import { useChainPool } from "hooks/useChainPool";
 import { useRouter } from "next/router";
-// import { PoolAllTotalDataProvider } from "components/Provider/PoolAllTotalDataProvider";
-// import { useAppData } from "context/AppDataContext";
-// import { POOL_CONFIG_MAP } from "constants/pools";
-// import useTotalPoolData from "hooks/pool/shared/useTotalPoolData";
 
 const Pool = () => {
   const router = useRouter();
   const { setChainId, setPoolName } = useChainPool();
   const { chain } = useNetwork();
-  // const { totalPoolData, updateTotalPoolData } = useAppData();
 
   useEffect(() => {
     if (chain) {
       setChainId(chain.id);
-      // Object.values(POOL_CONFIG_MAP[chain.id]).map((data, index) => {
-      // const { totalPoolData, error } = useTotalPoolData(data);
-      // const tokenData = data.baseToken;
-      // const symbol = tokenData?.symbol ?? "";
-      // if(totalPoolData) updateTotalPoolData(symbol,totalPoolData);
-      // })
-      // console.log(totalPoolData);
     }
   }, [chain, setChainId]);
 
