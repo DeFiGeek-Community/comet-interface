3371eb1410f576520e04a783a0e2680e18b00a6c
naizo10 <naizo018767@gmail.com>
2024-04-01 16:56:49 +0900

    Refactor pool.tsx to use useChainPool hook

--- pages/pool.tsx
+++ pages/pool.tsx
@@ -1,50 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useNetwork } from "wagmi";
-import { HashLoader } from "react-spinners";
-import { Center } from "utils/chakraUtils";
 import PoolPage from "components/PoolPage";
-import { ReloadContextProvider } from "components/Provider/ReloadContextProvider";
-import { PoolContext } from "context/PoolContext";
+import { useChainPool } from "hooks/useChainPool";
 import { useRouter } from "next/router";
 
 const Pool = () => {
-  const { chain } = useNetwork();
   const router = useRouter();
-  const [chainId, setChainId] = useState<number>(chain?.id || 1);
-  const [poolName, setPoolName] = useState<string>("");
+  const { setChainId, setPoolName } = useChainPool();
+  const { chain } = useNetwork();
 
   useEffect(() => {
     if (chain) {
       setChainId(chain.id);
     }
-  }, [chain]);
+  }, [chain, setChainId]);
 
   useEffect(() => {
     if (router.isReady) {
       setPoolName(router.query.pool ? (router.query.pool as string) : "CJPY");
     }
-  }, [router.isReady, router.query.pool]);
-
-  const [isRendered, setIsRendered] = useState(false);
-
-  useEffect(() => {
-    setIsRendered(true);
-  }, []);
+  }, [router.isReady, router.query.pool, setPoolName]);
 
   return (
-    <PoolContext.Provider
-      value={{ chainId, poolName, setChainId, setPoolName }}
-    >
-      <ReloadContextProvider>
-        {isRendered && router.isReady ? (
           <PoolPage />
-        ) : (
-          <Center height="100vh">
-            <HashLoader color="#FFF" />
-          </Center>
-        )}
-      </ReloadContextProvider>
-    </PoolContext.Provider>
   );
 };
 
