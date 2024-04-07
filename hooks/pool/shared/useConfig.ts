939141df49216e80549af2d8e12bdee1392f62f1
naizo10 <naizo018767@gmail.com>
2024-04-07 14:24:20 +0900

    Add useConfig hook in shared pool hooks

---
+++ hooks/pool/shared/useConfig.ts
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+import { POOL_CONFIG_MAP } from "constants/pools";
+import { useChainPool } from "hooks/useChainPool";
+import { PoolConfigMapForList } from "interfaces/pool";
+
+const useConfig = () => {
+  const { chainId } = useChainPool();
+  const [config, setConfig] = useState<PoolConfigMapForList | undefined>();
+
+  useEffect(() => {
+    if (chainId) {
+      const config = POOL_CONFIG_MAP[chainId];
+      setConfig(config);
+    }
+  }, [chainId]);
+
+  return config;
+};
+
+export default useConfig;
