9df83817c59b716723c5cf88ec5d8fd3a89a139e
yosuke <yosuke2002jp@yahoo.co.jp>
2024-05-19 20:23:49 +0900

    remove RenderStatusBarGray in StatusBarGray

--- components/pool/StatusBarGray.tsx
+++ components/pool/StatusBarGray.tsx
@@ -1,14 +1,10 @@
 import React from "react";
 import { StatusBarContainer, ProgressFillGrayOut } from "constants/rario";
 
-export const RenderStatusBarGray = () => {
-  return <ProgressFillGrayOut />;
-};
-
 const StatusBarGray = () => {
   return (
     <StatusBarContainer>
-      <RenderStatusBarGray />
+      <ProgressFillGrayOut />
     </StatusBarContainer>
   );
 };
