9a85a1c0157680f394674fa35991d0c2b083d469
yosuke <yosuke2002jp@yahoo.co.jp>
2024-06-13 17:22:38 +0900

    add AppSecondaryDataContext

---
+++ components/Provider/AppSecondaryDataProvider.tsx
@@ -0,0 +1,138 @@
+import React, { useState, ReactNode, useEffect, useRef } from "react";
+import { useNetwork } from "wagmi";
+import {
+  AppDataContext,
+  Currency,
+  AppDataContextType,
+} from "context/AppDataContext";
+import { PriceFeedData } from "hooks/pool/shared/usePriceFeed";
+import { TotalPoolData } from "hooks/pool/shared/useTotalPoolData";
+import { PoolConfigMapForList } from "interfaces/pool";
+import { POOL_CONFIG_MAP } from "constants/pools";
+import { BaseAssetData } from "../../hooks/pool/indivisual/useBaseAsset";
+import { CollateralAssetsData } from "../../hooks/pool/indivisual/useCollateralAssets";
+import { TokenRewardData } from "../../hooks/pool/shared/useTokenReward";
+
+export const AppSecondaryDataProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [pageName, setPageName] = useState<string>("");
+  const [chainId, setChainId] = useState<number>(0);
+  const [config, setConfig] = useState<PoolConfigMapForList | undefined>();
+  const { chain } = useNetwork();
+  const isFirstRender = useRef(true);
+
+  useEffect(() => {
+    if (chain) {
+      setConfig(POOL_CONFIG_MAP[chain.id]);
+      setChainId(chain.id);
+    } else {
+      setConfig(POOL_CONFIG_MAP[1]);
+    }
+  }, [chain, setChainId]);
+
+  const [priceFeedData, setPriceFeedData] = useState<{
+    [poolName: string]: PriceFeedData | undefined;
+  }>({});
+  const [totalPoolData, setTotalPoolData] = useState<{
+    [poolName: string]: TotalPoolData | undefined;
+  }>({});
+  const [baseAssetData, setBaseAssetData] = useState<{
+    [poolName: string]: BaseAssetData | undefined;
+  }>({});
+  const [collateralAssetsData, setCollateralAssetsData] = useState<{
+    [poolName: string]: CollateralAssetsData | undefined;
+  }>({});
+  const [tokenRewardData, setTokenRewardData] = useState<{
+    [poolName: string]: TokenRewardData | undefined;
+  }>({});
+
+  const [usdjpyPrice, setUsdjpyPrice] = useState<number | undefined>(undefined);
+
+  useEffect(() => {
+    // 初回のレンダリングをスキップ
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      return;
+    }
+
+    // addressが存在し、初回のレンダリングではない場合にreloadを実行
+    if (chain) {
+      setPriceFeedData({});
+      setTotalPoolData({});
+    }
+  }, [chain]);
+
+  const updatePriceFeedData = (
+    poolName: string,
+    data: PriceFeedData | undefined,
+  ) => {
+    setPriceFeedData((prevData) => ({ ...prevData, [poolName]: data }));
+    setUsdjpyPrice(data?.usdjpy);
+  };
+
+  const updateTotalPoolData = (
+    poolName: string,
+    data: TotalPoolData | undefined,
+  ) => {
+    setTotalPoolData((prevData) => ({ ...prevData, [poolName]: data }));
+  };
+
+  const updateBaseAssetData = (
+    poolName: string,
+    data: BaseAssetData | undefined,
+  ) => {
+    setBaseAssetData((prevData) => ({ ...prevData, [poolName]: data }));
+  };
+
+  const updateCollateralAssetsData = (
+    poolName: string,
+    data: CollateralAssetsData | undefined,
+  ) => {
+    setCollateralAssetsData((prevData) => ({ ...prevData, [poolName]: data }));
+  };
+
+  const updateTokenRewardData = (
+    poolName: string,
+    data: TokenRewardData | undefined,
+  ) => {
+    setTokenRewardData((prevData) => ({ ...prevData, [poolName]: data }));
+  };
+
+  const [currency, setCurrency] = useState<Currency>("USD");
+  const [rate, setRate] = useState<number>(1);
+
+  const toggleCurrency = () => {
+    if (usdjpyPrice !== undefined) {
+      setCurrency((prevCurrency) => {
+        const newCurrency = prevCurrency === "USD" ? "JPY" : "USD";
+        setRate(newCurrency === "JPY" ? usdjpyPrice : 1);
+        return newCurrency;
+      });
+    }
+  };
+
+  const value: AppDataContextType = {
+    pageName,
+    setPageName,
+    chainId,
+    config,
+    priceFeedData,
+    updatePriceFeedData,
+    totalPoolData,
+    updateTotalPoolData,
+    baseAssetData,
+    updateBaseAssetData,
+    collateralAssetsData,
+    updateCollateralAssetsData,
+    tokenRewardData,
+    updateTokenRewardData,
+    currency,
+    rate,
+    toggleCurrency,
+  };
+
+  return (
+    <AppDataContext.Provider value={value}>{children}</AppDataContext.Provider>
+  );
+};
