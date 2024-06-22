21c9151825f3abdfa999c1f57ddf196e0fb5fe6c
yosuke <yosuke2002jp@yahoo.co.jp>
2024-06-22 20:57:48 +0900

    modify PoolTable and add some translations

--- components/list/PoolTable.tsx
+++ components/list/PoolTable.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Heading, Text } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Heading, Text, Box, } from "@chakra-ui/react";
 import { Column, Row, useIsMobile } from "utils/chakraUtils";
 import DashboardBox from "components/shared/DashboardBox";
 import { useTranslation } from "react-i18next";
@@ -26,6 +26,7 @@ function RenderPoolTableRow() {
 interface TableHeaderColumnProps {
   text: string;
   width: string;
+  hovertext?: string;
 }
 
 const TableHeaderColumnBase: React.FC<TableHeaderColumnProps> = ({
@@ -47,22 +48,45 @@ const TableHeaderColumnBase: React.FC<TableHeaderColumnProps> = ({
 const TableHeaderColumn: React.FC<TableHeaderColumnProps> = ({
   text,
   width,
-}) => (
-  <Row
-    mainAxisAlignment="center"
-    crossAxisAlignment="center"
-    height="100%"
-    width={width}
-  >
-    <Text textAlign="center" fontWeight="bold" width="100%">
-      {text}
-    </Text>
-  </Row>
-);
+  hovertext,
+}) => {
+  const [isHovered, setIsHovered] = useState(false);
+  return (
+    <Row
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      mainAxisAlignment="center"
+      crossAxisAlignment="center"
+      height="100%"
+      width={width}
+    >
+      <Text textAlign="center" fontWeight="bold" width="100%">
+        {text}
+      </Text>
+      {hovertext && isHovered && (
+        <Box
+          position="absolute"
+          bg="gray.700"
+          p={2}
+          mt={-20}
+          boxShadow="md"
+          borderRadius="md"
+          zIndex="tooltip"
+        >
+          {hovertext}
+        </Box>
+      )}
+    </Row>
+  );
+};
 
 const PoolTable = () => {
   const { t } = useTranslation();
   const isMobile = useIsMobile();
+
+  const utilizationFormula = t("Borrow") + " / " + t("Supply") + " × 100";
+  const netEarnAPRFormula = t("Supply APR") + " + " + t("PND Bonus APR");
+  const netBorrowAPRFormula = t("Borrow APR") + " - " + t("PND Reward APR");
 
   return (
     <>
@@ -104,9 +128,9 @@ const PoolTable = () => {
                 >
                   <TableHeaderColumnBase text={t("Base Asset")} width="100%" />
                 </Row>
-                <TableHeaderColumn text={t("Utilization")} width="12%" />
-                <TableHeaderColumn text={t("Net Earn APR")} width="12%" />
-                <TableHeaderColumn text={t("Net Borrow APR")} width="12%" />
+                <TableHeaderColumn text={t("Utilization")} width="12%" hovertext={utilizationFormula}/>
+                <TableHeaderColumn text={t("Net Earn APR")} width="12%" hovertext={netEarnAPRFormula}/>
+                <TableHeaderColumn text={t("Net Borrow APR")} width="12%" hovertext={netBorrowAPRFormula}/>
                 <TableHeaderColumn
                   text={t("Total Supply Balance")}
                   width="12%"
