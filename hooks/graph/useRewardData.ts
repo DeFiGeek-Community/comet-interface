9e53fa4b8df41df014ebebd6ee4870296884033f
yosuke <yosuke2002jp@yahoo.co.jp>
2025-02-08 14:58:40 +0900

    restore calculateTotalBalance and calculateRewardData

--- hooks/graph/useRewardData.ts
+++ hooks/graph/useRewardData.ts
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import usePoolData from "hooks/pool/usePoolData";
 import { PoolConfig } from "interfaces/pool";
+import { RewardDataProps } from "interfaces/graph";
 
 const useRewardData = ({ poolData }: { poolData: PoolConfig }) => {
   const [tempRewardData, setTempRewardData] = useState({
