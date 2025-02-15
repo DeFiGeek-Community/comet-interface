1cda3c210b2ea83be6b870dfd1f042267cfd1678
yosuke <yosuke2002jp@yahoo.co.jp>
2025-02-15 14:32:11 +0900

    restore useRewardData

--- hooks/graph/useRewardData.ts
+++ hooks/graph/useRewardData.ts
@@ -2,9 +2,10 @@ import { useEffect, useState } from "react";
 import usePoolData from "hooks/pool/usePoolData";
 import { PoolConfig } from "interfaces/pool";
 import { RewardDataProps } from "interfaces/graph";
+import { calculateTotalBalance, calculateRewardData } from "hooks/util/graph";
 
 const useRewardData = ({ poolData }: { poolData: PoolConfig }) => {
-  const [tempRewardData, setTempRewardData] = useState({
+  const [tempRewardData, setTempRewardData] = useState<RewardDataProps>({
     borrow: undefined,
     earn: undefined,
   });
@@ -21,7 +22,30 @@ const useRewardData = ({ poolData }: { poolData: PoolConfig }) => {
       poolData?.baseMinForRewards
         ? totalPoolData?.totalBaseBorrowBalance ?? 0
         : 0;
+    const totalBaseSupplyBalance = calculateTotalBalance(
+      totalSupply,
+      priceFeedData,
+    );
+    const totalBaseBorrowBalance = calculateTotalBalance(
+      totalBorrow,
+      priceFeedData,
+    );
 
+    const tempSupplyRewardData = calculateRewardData(
+      poolData.baseTrackingRewardSpeed,
+      totalBaseSupplyBalance,
+      priceFeedData?.rewardAsset,
+    );
+    const tempBorrowRewardData = calculateRewardData(
+      poolData.baseTrackingRewardSpeed,
+      totalBaseBorrowBalance,
+      priceFeedData?.rewardAsset,
+    );
+
+    setTempRewardData({
+      borrow: tempBorrowRewardData,
+      earn: tempSupplyRewardData,
+    });
   }, [priceFeedData, totalPoolData]);
 
   return tempRewardData;
